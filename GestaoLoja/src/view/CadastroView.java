141d15e6dd26e925df1f78f1b0292871c1ef9663
TarcioR <tarcio.rodrigues@alunos.ifsuldeminas.edu.br>
2023-06-23 21:13:26 -0300

    correção cadastro
    correção do botao cadastro e tamanho do menu

--- GestaoLoja/src/view/CadastroView.java
+++ GestaoLoja/src/view/CadastroView.java
@@ -1,6 +1,7 @@
 package view;
 
 import dao.UsuarioDAO;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,6 +11,7 @@ import model.Usuario;
 public class CadastroView extends JFrame {
     private JTextField textFieldUsuario;
     private JPasswordField passwordFieldSenha;
+    private JComboBox<String> comboBoxNivelAcesso;
     private JButton buttonCadastrar;
 
     public CadastroView() {
@@ -22,13 +24,17 @@ public class CadastroView extends JFrame {
         setResizable(false);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 2, 10, 10));
+        panel.setLayout(new GridLayout(5, 2, 10, 10));
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
         JLabel labelUsuario = new JLabel("Usuário:");
         textFieldUsuario = new JTextField();
         JLabel labelSenha = new JLabel("Senha:");
         passwordFieldSenha = new JPasswordField();
+        JLabel labelNivelAcesso = new JLabel("Nível de Acesso:");
+        comboBoxNivelAcesso = new JComboBox<>();
+        comboBoxNivelAcesso.addItem("Usuário");
+        comboBoxNivelAcesso.addItem("Administrador");
         buttonCadastrar = new JButton("Cadastrar");
 
         buttonCadastrar.addActionListener(new ActionListener() {
@@ -36,9 +42,10 @@ public class CadastroView extends JFrame {
             public void actionPerformed(ActionEvent evt) {
                 String usuario = textFieldUsuario.getText();
                 String senha = String.valueOf(passwordFieldSenha.getPassword());
+                String nivelAcesso = comboBoxNivelAcesso.getSelectedItem().toString();
 
                 // cadastra o usuário
-                Usuario novoUsuario = new Usuario(usuario, senha, "usuario");
+                Usuario novoUsuario = new Usuario(usuario, senha, nivelAcesso);
                 UsuarioDAO usuarioDAO = new UsuarioDAO();
                 usuarioDAO.cadastrarUsuario(novoUsuario);
 
@@ -49,12 +56,12 @@ public class CadastroView extends JFrame {
             }
         });
 
-
-
         panel.add(labelUsuario);
         panel.add(textFieldUsuario);
         panel.add(labelSenha);
         panel.add(passwordFieldSenha);
+        panel.add(labelNivelAcesso);
+        panel.add(comboBoxNivelAcesso);
         panel.add(new JLabel());
         panel.add(buttonCadastrar);
 
